b6b68cd11445132a071f9454ed25e2cba9f9cb3a
iska000s <izabelast@op.pl>
2023-07-22 10:44:07 +0200

    Configuration

---
+++ unnecessary-items/src/App.js
@@ -0,0 +1,19 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Layout
+      </header>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
